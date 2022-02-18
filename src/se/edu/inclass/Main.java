4bfa1e2ee1b038da82ec2688f5eef19635931d90
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-02-18 17:20:45 +0800

    Added methods that work with streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -38,6 +38,29 @@ public class Main {
         }
     }
 
+    public static void printDataWithStreams(ArrayList<Task> tasks) {
+        System.out.println("\nPrint tasks using streams");
+        tasks.stream()  //convert task data to a stream
+                //parallelStream will split the data between CPUs I believe
+                .forEach(System.out::println);  //terminal operation
+    }
+
+    public static void printDeadlinesWithStreams(ArrayList<Task> tasks) {
+        System.out.println("\n Print deadlines using streams");
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .forEach(System.out::println);
+    }
+
+    private static int countDeadlinesWithStream(ArrayList<Task> tasks) {
+        int count = 0;
+        count = (int)tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+
+        return count;
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
